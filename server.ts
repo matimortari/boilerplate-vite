ae221847bc34cf0d88f8c6160374fd3bfe460796
matheus <matheus.felipe.19rt@gmail.com>
2024-07-13 20:31:10 -0300

    chore: Update CSS & Tailwind classes

--- server.ts
+++ server.ts
@@ -4,9 +4,11 @@ import express from "express"
 import mongoose from "mongoose"
 
 dotenv.config()
+
 const app = express()
 app.use(express.json())
 app.use(cors())
+
 mongoose
 	.connect(process.env.MONGO_URL!)
 	.then(() => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,6 @@ const config = {
 			colors: {
 				background: "var(--background)",
 				foreground: "var(--foreground)",
-				card: {
-					DEFAULT: "var(--card)",
-					foreground: "var(--card-foreground)",
-				},
-				popover: {
-					DEFAULT: "var(--popover)",
-					foreground: "var(--popover-foreground)",
-				},
 				primary: {
 					DEFAULT: "var(--primary)",
 					foreground: "var(--primary-foreground)",
@@ -48,20 +40,6 @@ const config = {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
-			},
-			keyframes: {
-				"accordion-down": {
-					from: { height: "0" },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: "0" },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
 			},
 		},
 	},
