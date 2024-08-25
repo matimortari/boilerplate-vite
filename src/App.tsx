f3c8be94182f1474ad2a7509818de1ba7a2034a2
matheus <matheus.felipe.19rt@gmail.com>
2024-08-25 17:29:45 -0300

    refactor: Refactor main App component structure | style: Add & use Inter font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
 	return (
-		<div className="flex h-screen items-center justify-center">
-			<strong className="text-4xl">Hello World!</strong>
-		</div>
+		<>
+			<main className="flex h-screen items-center justify-center">
+				<div>
+					<strong className="text-4xl">Hello World!</strong>
+				</div>
+			</main>
+		</>
 	)
 }
