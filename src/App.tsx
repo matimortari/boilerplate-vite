ecb9d3e7a5b9137110591219d32db16b1fa7cf43
matheus <matheus.felipe.19rt@gmail.com>
2024-08-31 15:19:32 -0300

    chore: Install & use Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Analytics } from "@vercel/analytics/react"
+
 export default function Home() {
 	return (
 		<>
+			<Analytics />
 			<main className="flex h-screen items-center justify-center">
 				<div>
 					<strong className="text-4xl">Hello World!</strong>
